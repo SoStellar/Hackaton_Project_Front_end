24736cac7a35723b0bdcb8c64dffb07d25dc8aa6
SoStellar <githubpkaddress@gmail.com>
2022-11-15 22:53:28 +0700

    opd-card done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar  from './Components/Navbar';
 import './App.css';
+import Opdcard from './Components/Opdcard';
 
 function App() {
   return (
     <>
     <Navbar />
+    <Opdcard />
     </>
   );
 }
