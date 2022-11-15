631556f344659e4db6d26c6633d9dc260fd45a4a
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-11-16 03:08:12 +0700

    function bar complete

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown, Stack, Row, Col } from 'react-bootstrap';
 import { ImProfile } from "react-icons/im";
 import { FaUserEdit } from "react-icons/fa";
 import { IoCalendar } from "react-icons/io5";
@@ -17,7 +17,7 @@ export default function CollapsibleExample() {
                             alt="logoSmileDent"
                         />
                     </Navbar.Brand>
-                    <Navbar.Brand href="#home" style={{ fontSize: "48px",color: "#6892D5" }} >SmileDent.</Navbar.Brand>
+                    <Navbar.Brand href="#home" style={{ fontSize: "48px", color: "#6892D5" }} >SmileDent.</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="justify-content-end flex-grow-1 pe-5">
@@ -26,16 +26,15 @@ export default function CollapsibleExample() {
                             <Nav.Link href="#appiontment"><IoCalendar /> ตารางนัด</Nav.Link>
                         </Nav>
                         <Navbar.Brand href="#Profile">
-                                <img
-                                    src={require('../Picture/exProfile.png')}
-                                    width="70"
-                                    height="70"
-                                    className="d-inline-block align-top"
-                                    alt="logoSmileDent"
-                                />
-                            </Navbar.Brand>
+                            <img
+                                src={require('../Picture/exProfile.png')}
+                                width="70"
+                                height="70"
+                                className="d-inline-block align-top"
+                                alt="logoSmileDent"
+                            />
+                        </Navbar.Brand>
                         <Nav>
-                            
                             <NavDropdown title="User" id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="#action1">Logout</NavDropdown.Item>
                             </NavDropdown>
@@ -43,6 +42,28 @@ export default function CollapsibleExample() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <Container fluid className={"font-link"} style={{ height: "62.5rem"}} >
+                <Row >
+                    <Col md={2}  style={{ width: "7.5rem",}} >
+                        <Stack gap={0} className="text-center mt-5">
+                            <Nav.Link href="#treatment" className='border-info rounded-top pt-5' style={{ height: "7.5rem", backgroundColor: "#C9FDD7", color: "#6892D5" }}>
+                                ข้อมูลการรักษา
+                            </Nav.Link>
+                            <Nav.Link href="#Picture" className="border-top border-info pt-5" style={{ height: "7.5rem",backgroundColor: "#C9FDD7", color: "#6892D5" }}>
+                                X-ray flim
+                            </Nav.Link>
+                            <Nav.Link href="#Contact" className="border-top border-info rounded-bottom pt-5" style={{ height: "7.5rem",backgroundColor: "#C9FDD7", color: "#6892D5" }}>
+                                ข้อมูลส่วนตัวคนไข้
+                            </Nav.Link>
+                        </Stack>
+                    </Col>
+                    <Col md={6} >2 of 3</Col>
+                    <Col md={4}>3 of 3</Col>
+                </Row>
+                </Container>
+
+            
+
         </>
     );
-}
+};
