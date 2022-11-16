f8c2bb6408fa1256a58fb39daec9ad0baeace355
SoStellar <githubpkaddress@gmail.com>
2022-11-16 12:20:18 +0700

    xrayfilm finish

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Navbar  from './Components/Navbar';
 import './App.css';
 import Opdcard from './Components/Opdcard';
 import SearchPatient from './Components/SearchPatient';
+import XRayFilm from './Components/XRayFilm';
 
 function App() {
   return (
     <>
     <Navbar />
-    <SearchPatient />
-    <Opdcard />
+    {/* <SearchPatient />
+    <Opdcard /> */}
+    <XRayFilm />
     </>
   );
 }
