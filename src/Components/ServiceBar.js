d00aa4f0888367c80f9d1695ff7d5df8e4ae4a64
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-11-17 00:09:58 +0700

    add shadow

--- src/Components/ServiceBar.js
+++ src/Components/ServiceBar.js
@@ -7,14 +7,17 @@ export default function ServiceBar (){
         <Container fluid className={"font-link"} style={{ height: "62.5rem" }} >
                 <Row >
                     <Col md={2} style={{ width: "7.5rem", }} >
-                        <Stack gap={0} className="text-center mt-5 pt-5">
-                            <Nav.Link href="#treatment" className='border-info rounded-top pt-5' style={{ height: "7.5rem", backgroundColor: "#C9FDD7", color: "#6892D5" }}>
+                        <Stack gap={0} className="text-center shadow mt-5 pt-5">
+                            <Nav.Link href="#treatment" className='border-info shadow rounded-top pt-5' 
+                            style={{ height: "7.5rem", backgroundColor: "#C9FDD7", color: "#6892D5" }}>
                                 ข้อมูลการรักษา
                             </Nav.Link>
-                            <Nav.Link href="#Picture" className="border-top border-info pt-5" style={{ height: "7.5rem", backgroundColor: "#C9FDD7", color: "#6892D5" }}>
+                            <Nav.Link href="#Picture" className="border-top shadow border-info pt-5" 
+                            style={{ height: "7.5rem", backgroundColor: "#C9FDD7", color: "#6892D5" }}>
                                 X-ray flim
                             </Nav.Link>
-                            <Nav.Link href="#Contact" className="border-top border-info rounded-bottom pt-5" style={{ height: "7.5rem", backgroundColor: "#C9FDD7", color: "#6892D5" }}>
+                            <Nav.Link href="#Contact" className="border-top shadow border-info rounded-bottom pt-5" 
+                            style={{ height: "7.5rem", backgroundColor: "#C9FDD7", color: "#6892D5" }}>
                                 ข้อมูลส่วนตัวคนไข้
                             </Nav.Link>
                         </Stack>
