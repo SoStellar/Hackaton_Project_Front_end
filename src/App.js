38b53700268214feadff3a0022956786109baf94
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-11-21 04:34:07 +0700

    add Nav bar routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import TX from './Components/TX';
 import FormPatient from './Components/FormPatient';
 import XRayFilm from './Components/XRayFilm';
 import Opdcard from './Components/Opdcard';
+import Appointment from './Components/Appointment';
 
 
 function App() {
@@ -15,6 +16,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<TX />} />
+        {/* Routes Navbar */}
+        <Route path="/registerPatient" element={<FormPatient />} />
+        <Route path="/patient" element={<TX />} />
+        <Route path="/appointment" element={<Appointment />} />
+        {/* Routes Service bar */}
         <Route path="/patient" element={<TX />} />
         <Route path="/picture" element={<XRayFilm />} />
         <Route path="/opdcard" element={<Opdcard />} />
