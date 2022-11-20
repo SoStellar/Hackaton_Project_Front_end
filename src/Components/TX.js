9faf4444eabbf28061b2471147f73459101e6066
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-11-20 23:03:55 +0700

    crop pic and edit text

--- src/Components/TX.js
+++ src/Components/TX.js
@@ -1,9 +1,10 @@
 import { React, useState } from 'react';
-import { Container, Stack, Nav, Row, Col, Tab, Tabs, Card, Button, Modal, ListGroup } from 'react-bootstrap';
+import { Container, Stack, Nav, Row, Col, Tab, Tabs, Card, Button, Modal, ListGroup, Form } from 'react-bootstrap';
 import HistoryTable from './TableHistory';
 import AddCaseTable from './TableAddCase';
 import Datepicker from './Datepicker';
 import { HiPlus } from 'react-icons/hi';
+import { Checkbox } from '@mobiscroll/react';
 
 
 
@@ -12,12 +13,14 @@ export default function TX() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    var Upper_teeth = document.getElementsByName('UpperTeeth') 
+
 
     return (
         <>
             <Container fluid className="font-link" style={{ height: "62.5rem" }} >
                 <Row >
-                    <Col md={2}  style={{ width: "7.5rem",marginTop: "4.5rem" }} >
+                    <Col md={2} style={{ width: "7.5rem", marginTop: "4.5rem" }} >
                         <Stack gap={0} className="text-center  mt-5 pt-5">
                             <Nav.Link href="#treatment" className='border-info rounded-top pt-5'
                                 style={{ height: "7.5rem", backgroundColor: "#C9FDD7", color: "#6892D5" }}>
@@ -48,85 +51,94 @@ export default function TX() {
                         </Card>
                         <Col md={6}>
                             <Container fluid className='border border-secondary  rounded mt-3 font-link'
-                             style={{ height: "35rem",width: "50rem"}}  >
+                                style={{ height: "35rem", width: "50rem" }}  >
                                 <Row >
-                                    <Col md={1} className="text-center" style={{
-                                        height: "31rem"}}>
-                                        Right
+                                    <Col md={2} className="d-flex align-items-start flex-column"
+                                        style={{ height: "35rem", color: "#6892D5" }}>
+                                        <div class="mb-auto p-2">Upper right</div>
+                                        <div class="p-2">Lower right</div>
                                     </Col>
-                                    <Col md={10} style={{ height: "31rem" }}>
+                                    <Col md={8} className="img-responsive center-block"
+                                        style={{ height: "35rem" }}>
+                                        <Form className='teethCheckbox'>
+                                            <idTeeth />
+                                        </Form>
 
+                                        <img src={require("../Picture/TeethDiagram.png")} class="img-fluid align-self-center" alt="TeethDiagram"
+                                            style={{}} />
                                     </Col>
-                                    <Col md={1} className="text-center" style={{ height: "31rem"}}>
-                                        Left
+                                    <Col md={2} className="d-flex align-items-end flex-column"
+                                        style={{ height: "35rem", color: "#6892D5" }}>
+                                        <div class="mb-auto p-2">Upper left</div>
+                                        <div class="p-2">Lower left</div>
                                     </Col>
                                 </Row>
                             </Container>
                         </Col>
-                </Col>
-                        <Col md={5}>
-                            <Tabs
-                                defaultActiveKey="history"
-                                id="TabsTx"
-                                style={{ width: "34rem", marginTop: "3.5rem" }}
+                    </Col>
+                    <Col md={5}>
+                        <Tabs
+                            defaultActiveKey="history"
+                            id="TabsTx"
+                            style={{ width: "34rem", marginTop: "3.5rem" }}
 
-                            >
-                                <Tab eventKey="history" title="History" >
-                                    <HistoryTable />
-                                </Tab>
-                                <Tab eventKey="addCase" title="Add case">
-                                    <AddCaseTable />
-                                    <Button className='bg-white text-center'
-                                        style={{
-                                            borderRadius: "3rem", borderColor: "#6892D5",
-                                            borderWidth: "2px", color: "#6892D5", marginLeft: "15rem"
-                                        }}
-                                        value={'input'}
-                                        onClick={handleShow}>
-                                        <HiPlus />
-                                    </Button>
-                                </Tab>
-                            </Tabs>
-                        </Col>
-                        <Modal
-                            show={show}
-                            onHide={handleClose}
-                            keyboard={false}
-                            size="lg"
-                            style={{ marginTop: "8rem" }}
                         >
-                            <Modal.Header closeButton>
-                                <Modal.Title style={{ color: "#6892D5" }}>Add Case</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body >
-                                <Container>
-                                    <Row>
-                                        <Col xs={12} md={8}>
-                                            <h2>Date picker</h2>
-                                            <Datepicker style={{ color: "#6892D5" }} />
-                                        </Col>
-                                        <Col xs={6} md={4}>
-                                            <Card className='font-link ms-5' style={{ width: '10rem', }}>
-                                                <ListGroup variant="flush">
-                                                    <ListGroup.Item style={{ color: "#79D1C3" }}>อุดฟัน</ListGroup.Item>
-                                                    <ListGroup.Item style={{ color: "#79D1C3" }}>ถอนฟัน</ListGroup.Item>
-                                                    <ListGroup.Item style={{ color: "#79D1C3" }}> ขูดหินปูน</ListGroup.Item>
-                                                    <ListGroup.Item style={{ color: "#79D1C3" }}>รักษารากฟัน</ListGroup.Item>
-                                                    <ListGroup.Item style={{ color: "#79D1C3" }}>ฟอกฟันขาว</ListGroup.Item>
-                                                    <ListGroup.Item style={{ color: "#79D1C3" }}>ผ่าฟันคุด</ListGroup.Item>
-                                                </ListGroup>
-                                            </Card>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </Modal.Body>
-                            <Modal.Footer>
-                                <Button variant="danger" onClick={handleClose}>
-                                    Close
+                            <Tab eventKey="history" title="History" >
+                                <HistoryTable />
+                            </Tab>
+                            <Tab eventKey="addCase" title="Add case">
+                                <AddCaseTable />
+                                <Button className='bg-white text-center'
+                                    style={{
+                                        borderRadius: "3rem", borderColor: "#6892D5",
+                                        borderWidth: "2px", color: "#6892D5", marginLeft: "15rem"
+                                    }}
+                                    value={'input'}
+                                    onClick={handleShow}>
+                                    <HiPlus />
                                 </Button>
-                                <Button variant="primary">Add</Button>
-                            </Modal.Footer>
-                        </Modal>
+                            </Tab>
+                        </Tabs>
+                    </Col>
+                    <Modal
+                        show={show}
+                        onHide={handleClose}
+                        keyboard={false}
+                        size="lg"
+                        style={{ marginTop: "8rem" }}
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title style={{ color: "#6892D5" }}>Add Case</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body >
+                            <Container>
+                                <Row>
+                                    <Col xs={12} md={8}>
+                                        <h2>Date picker</h2>
+                                        <Datepicker style={{ color: "#6892D5" }} />
+                                    </Col>
+                                    <Col xs={6} md={4}>
+                                        <Card className='font-link ms-5' style={{ width: '10rem', }}>
+                                            <ListGroup variant="flush">
+                                                <ListGroup.Item style={{ color: "#79D1C3" }}>อุดฟัน</ListGroup.Item>
+                                                <ListGroup.Item style={{ color: "#79D1C3" }}>ถอนฟัน</ListGroup.Item>
+                                                <ListGroup.Item style={{ color: "#79D1C3" }}> ขูดหินปูน</ListGroup.Item>
+                                                <ListGroup.Item style={{ color: "#79D1C3" }}>รักษารากฟัน</ListGroup.Item>
+                                                <ListGroup.Item style={{ color: "#79D1C3" }}>ฟอกฟันขาว</ListGroup.Item>
+                                                <ListGroup.Item style={{ color: "#79D1C3" }}>ผ่าฟันคุด</ListGroup.Item>
+                                            </ListGroup>
+                                        </Card>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="danger" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="primary">Add</Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Row>
             </Container>
         </>
