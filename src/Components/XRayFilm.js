40ec56f299dccf3d251eb68b1d6791a0aaa9e46f
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-11-20 17:16:16 +0700

    add service bar finish

--- src/Components/XRayFilm.js
+++ src/Components/XRayFilm.js
@@ -5,9 +5,9 @@ import { MdUploadFile } from 'react-icons/md'
 export default function XRayFilm() {
   return (
     <>
-      <Container fluid className="font-link" style={{ height: "62.5rem" }}>
+      <Container fluid className="font-link" >
         <Row>
-          <Col md={2} style={{ width: "7.5rem", marginTop: "4.5rem" }} >
+          <Col md={3} style={{ width: "7.5rem", marginTop: "4.5rem" }} >
             <Stack gap={0} className="text-center  mt-5 pt-5">
               <Nav.Link href="#treatment" className='rounded-top pt-5'
                 style={{ height: "7.5rem", backgroundColor: "#C9FDD7", color: "#6892D5" }}>
@@ -23,28 +23,30 @@ export default function XRayFilm() {
               </Nav.Link>
             </Stack>
           </Col>
-          <Col md={7}>
+          <Col md={6}>
             <Container fluid>
-              <h1 className="mb-5" style={{ color: "#6892D5", textShadow: "2px 2px 2px #79D1C3" }}>รูปภาพ/X-ray Flim</h1>
-              <Carousel className="d-flex justify-content-center " style={{ width : "60rem"}}>
+              <h1 className=" mt-5 mb-3" style={{ color: "#6892D5", textShadow: "2px 2px 2px #79D1C3" }}>
+                รูปภาพ/X-ray Flim
+                </h1>
+              <Carousel className="d-flex justify-content-center " style={{ width : "55rem"}}>
                 <Carousel.Item>
                   <img
                     src={require("../Picture/xrayfilm1.jpg")}
-                    className="d-flex w-85 fluid"
+                    className="d-flex w-100"
                     style={{ height: "30rem" }}
                   ></img>
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
                     src={require("../Picture/xrayfilm2.jpg")}
-                    className=" d-flex w-85 fluid"
+                    className=" d-flex w-100"
                     style={{ height: "30rem" }}
                   ></img>
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
                     src={require("../Picture/xrayfilm3.jpg")}
-                    className="d-flex w-85 fluid"
+                    className="d-flex w-100"
                     style={{ height: "30rem" }}
                   ></img>
                 </Carousel.Item>
@@ -53,15 +55,16 @@ export default function XRayFilm() {
           </Col>
         
 
-        <Col md={3}>
+        <Col md={3} className="ms-5">
           <Container fluid>
             <h1
-              className="d-flex justify-content-center"
+              className="d-flex justify-content-center mt-5"
               style={{ color: "#6892D5", textShadow: "2px 2px 2px #79D1C3" }}
             >
               Upload File
             </h1>
-            <Container className="border border-secondary  border-3 rounded mt-5" style={{ width: "26rem", height: "24rem", borderStyle: "dashed" }}>
+            <Container className="border border-secondary  border-3 rounded mt-3" 
+            style={{ width: "26rem", height: "24rem", borderStyle: "dashed" }}>
               <MdUploadFile  style={{ width: "15rem", height: "15rem", opacity: "30%",marginTop: "3rem",marginLeft: "5rem" }} />
             </Container>
 
