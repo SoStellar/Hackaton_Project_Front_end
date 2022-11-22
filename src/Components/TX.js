6c3fbd20b8cb38fd83c32210e017ea2cc9d4c282
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-11-22 13:24:26 +0700

    edit position modal - size select

--- src/Components/TX.js
+++ src/Components/TX.js
@@ -12,6 +12,9 @@ export default function TX() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    
+
+
     const [serviceList, setServiceList] = useState([{ id: "อุดฟัน", value: "อุดฟัน" }, { id: "ถอนฟัน", value: "ถอนฟัน" }, { id: "ขูดหินปูน", value: "ขูดหินปูน" },
     { id: "รักษารากฟัน", value: "รักษารากฟัน" }, { id: "ฟอกฟันขาว", value: "ฟอกฟันขาว" }])
     const [teethCodeList, setTeethCodeList] = useState([{ id: 1, value: 1 }, { id: 2, value: 2 }, { id: 3, value: 3 }, { id: 4, value: 4 }, { id: 5, value: 5 }, { id: 6, value: 6 },
@@ -19,6 +22,8 @@ export default function TX() {
     { id: 13, value: 13 }, { id: 14, value: 14 }, , { id: 15, value: 15 }, { id: 16, value: 16 }, { id: 17, value: 17 }, { id: 18, value: 18 },
     { id: 19, value: 19 }, { id: 20, value: 20 }, { id: 21, value: 22 }, { id: 23, value: 23 }, { id: 24, value: 24 }, { id: 25, value: 25 },
     { id: 26, value: 26 }, { id: 27, value: 27 }, { id: 28, value: 28 }, { id: 29, value: 29 }, { id: 30, value: 30 }, { id: 31, value: 31 }, { id: 32, value: 32 }])
+
+
 
     const IdTeethCode = () => {
         return (
@@ -132,7 +137,7 @@ export default function TX() {
                             onHide={handleClose}
                             keyboard={false}
                             size="xl"
-                            style={{ marginTop: "8rem" }}
+                            
                         >
                             <Modal.Header closeButton>
                                 <Modal.Title style={{ color: "#6892D5" }}>Add Case</Modal.Title>
@@ -142,7 +147,7 @@ export default function TX() {
                                     <Row>
                                         <Col xs={12} md={8}>
                                             <h3 className="mb-3 font-link" style={{ color: "#79D1C3", textShadow: "1px 1px 1px #6892D5" }} >วันที่</h3>
-                                            <Datepicker style={{ color: "#6892D5" }} />
+                                            <Datepicker id="date" style={{ color: "#6892D5" }} />
                                             <Container fluid className='border border-primary  rounded mt-3 font-link'
                                                 style={{ height: "35rem", width: "40rem" }}  >
                                                 <Row >
@@ -169,7 +174,7 @@ export default function TX() {
                                                     borderWidth: "2px", color: "white",
                                                 }}
                                                 value={'input'}
-                                                onClick={handleShow}>
+                                                onClick={<IdTeethCode />}>
                                                 <HiPlus />
                                             </Button>
                                             <h3 className="mb-3 font-link" style={{ color: "#79D1C3", textShadow: "1px 1px 1px #6892D5" }} >บริการ</h3>
@@ -177,7 +182,7 @@ export default function TX() {
                                             className='pre-scrollable font-link fw-bold'
                                             aria-label="Default select example" 
                                             style={{ backgroundColor: "#C9FDD7", borderColor: "#79D1C3",
-                                             color: "#5F6C72", maxHeight: "150px", overflowY: "auto",width: "9.5rem" }}>
+                                             color: "#5F6C72", maxHeight: "100px", overflowY: "auto",width: "9.5rem" }}>
                                                  <option>Select service</option>
                                                 <IdService />
                                             </Form.Select>
